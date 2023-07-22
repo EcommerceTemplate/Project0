3149070150092d862610e9961e9e8de42aaf4dc8
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-07-21 22:04:11 -0300

    chore: change start proyects files

--- src/main/java/com/project0/application/Application.java
+++ src/main/java/com/project0/application/Application.java
@@ -1,13 +1,13 @@
-package com.project0.demo;
+package com.project0.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/project0/application/ApplicationTest.java
+++ src/test/java/com/project0/application/ApplicationTest.java
@@ -1,10 +1,10 @@
-package com.project0.demo;
+package com.project0.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTest {
 
 	@Test
 	void contextLoads() {
